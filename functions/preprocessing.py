22af9ffb04116bb6ad20f91476d725d8d9593ae7
Joan <joancaballero8@gmail.com>
2024-12-10 20:13:04 +0100

    New Feature + Preprocessing

--- functions/preprocessing.py
+++ functions/preprocessing.py
@@ -3,8 +3,33 @@ import nltk
 
 wnl = nltk.stem.WordNetLemmatizer()
 
-def remove_punctuation_tokens(tokens):
-    return [token for token in tokens if token not in string.punctuation]
+def replace_contractions(tokens):
+  """
+  Replace specified contractions in a list of tokens, as specified by TakeLab.
+
+  Parameters:
+    tokens (list): The list of tokens to process.
+
+  Returns:
+    list: The list of tokens after replacing the specified contractions.
+  """
+  contractions_dict = {
+      "n’t": "not", "n't": "not",
+      "’m": "am", "'m": "am",
+      "’ve": "have", "'ve": "have",
+      "’re": "are", "'re": "are",
+      "’ll": "will", "'ll": "will",
+  }
+  
+  new_tokens = []
+  for t in tokens:
+      lower_t = t.lower()
+      if lower_t in contractions_dict:
+          new_tokens.append(contractions_dict[lower_t])
+      else:
+          new_tokens.append(t)
+  return new_tokens
+
 
 # Function to lemmatize based on POS tagging
 def lemmatize_WNL(p, warn=False):
@@ -23,7 +48,7 @@ def lemmatize_WNL(p, warn=False):
 
 # Function to POS tag and lemmatize tokens in a sentence
 def lemmatize(row, name, warn=False):
-  tokens = row[name]
-  tokens = [token.lower() for token in tokens]
-  pair_tags = nltk.pos_tag(tokens)
-  return [lemmatize_WNL(pair, warn) for pair in pair_tags]
+    tokens = row[name]
+    tokens = [token.lower() for token in tokens]
+    pair_tags = nltk.pos_tag(tokens)
+    return [lemmatize_WNL(pair, warn) for pair in pair_tags]
